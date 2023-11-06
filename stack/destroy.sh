56f56b6992860efda650cbd6b907b75cc208f973
dykoffi <dykoffi2000@gmail.com>
2023-11-06 14:55:34 +0000

    I Hope it will work

--- stack/destroy.sh
+++ stack/destroy.sh
@@ -1,14 +1,11 @@
 namespace=motower-app
 
-## Desintaller keycloak
-kubectl delete -f keycloak.yml
-
 ## Desinstaller la chart postgres
 helm uninstall postgres --namespace $namespace || true
 
-## Desinstaller la chart postgres de keycloak
-helm uninstall keycloak-postgres --namespace $namespace || true
+## Desinstaller la chart keycloak
+helm uninstall keycloak --namespace $namespace || true
 
 ## Delete PVCs
-kubectl delete  pvc data-keycloak-postgres-postgresql-0 --namespace $namespace
+kubectl delete  pvc data-keycloak-postgresql-0 --namespace $namespace
 kubectl delete  pvc data-postgres-postgresql-0 --namespace $namespace
--- stack/run.sh
+++ stack/run.sh
@@ -10,10 +10,10 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 ## Installer la chart postgres
 helm upgrade --install postgres bitnami/postgresql --namespace $namespace --create-namespace --version "12.11.1" -f $(pwd)/postgres_values.yml
 
-## Installer la chart postgres for keycloak
-helm upgrade --install keycloak-postgres bitnami/postgresql --namespace $namespace --create-namespace --version "12.11.1" -f $(pwd)/keycloak_postgres_values.yml
+## Installer la chart keycloak
+helm upgrade --install keycloak bitnami/keycloak --namespace $namespace --create-namespace --version "13.0.5" -f $(pwd)/keycloak_values.yml
 
-keycloak_pod=$(kubectl get po --namespace $namespace --selector=app=keycloak --template='{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+keycloak_pod=keycloak-0
 
 ## Attendre que le pod de keycloak soit pret
 kubectl wait --timeout=-1s --for=condition=Ready pod/$keycloak_pod -n $namespace
